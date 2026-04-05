0b6614196e6c1a9f5cc2a0afa96400e182f4b722
Tesfamichael-G <tesfamichael.gegziabher@aait.edu.et>
2026-04-05 14:26:09 +0300

    Update SmartLight.java

--- src/main/java/com/jju/SmartLight.java
+++ src/main/java/com/jju/SmartLight.java
@@ -1,1 +1,13 @@
+package com.jju;
+
+public class SmartLight extends SmartDevice {
+    public SmartLight(String name) {
+        super(name);
+    }
+
+    @Override
+    public void activate() {
+        // TODO: Print "[Name] is now ON at 100% brightness."
+    }
+}
 
